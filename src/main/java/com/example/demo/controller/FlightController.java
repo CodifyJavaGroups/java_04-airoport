a17d3a181943c20a35eaccfa8c825552975bc34c
Sultan-gofer <goferagims@gmail.com>
2024-12-15 23:12:18 +0600

    Добавил Контроллер рейсов и сваггер и дто для Рейсов

---
+++ src/main/java/com/example/demo/controller/FlightController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.flightdtos.FlightDtoAdd;
+import com.example.demo.dto.flightdtos.FlightResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping(name = "/flight")
+@Tag(name = "Контроллер для получения инфомации на рейсах")
+public class FlightController {
+
+    @Operation(
+            summary = "Получение всех рейсов в системе",
+            description = "Возвращает лист рейсов",
+            responses ={
+                    @ApiResponse(responseCode = "404",
+                            description = "Ещё не сделан")
+            }
+    )
+    @GetMapping(name = "/getAll")
+    public List<FlightResponseDto> getAllFlight() {
+        return Collections.emptyList();
+    }
+
+    @Operation(
+            summary = "Получает лист рейсов по фильтру",
+            responses ={
+                    @ApiResponse(responseCode = "404",
+                    description = "Ещё не сделан")
+            }
+    )
+    @GetMapping(value = "/getByfilter")
+    public List<FlightResponseDto> getFlightByFilter(
+            @RequestParam String flightNumber,
+            @RequestParam String flightDateDeparture,
+            @RequestParam String flightDateArrival,
+            @RequestParam String status,
+            @RequestParam String placeOfDeparture,
+            @RequestParam String placeOfArrival
+    ){
+    return Collections.emptyList();
+    }
+
+    @Operation(
+            summary = "Добавить новый рейс",
+            description = "Отправляется FlightDtoAdd в котором нужно указывать с какого аиропорта прилетают с помощью " +
+                    "id(таким же методом и куда улетают) ",
+            responses ={
+                    @ApiResponse(responseCode = "404",
+                            description = "Ещё не сделан")
+            }
+    )
+    @PostMapping(value = "/add-flight")
+    public FlightResponseDto addFlight(@RequestBody FlightDtoAdd flightDtoAdd){
+        return null;
+    }
+}
