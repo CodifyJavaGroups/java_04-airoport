2808a98a37175cb919eb374eb7d4991305aa28ff
Igor Solodovnikov <igor16312@gmail.com>
2024-12-16 18:30:17 +0600

    Dto added

--- src/main/java/com/example/demo/mapper/EmployeeEntityToDto.java
+++ src/main/java/com/example/demo/mapper/EmployeeEntityToDto.java
@@ -10,8 +10,8 @@ public class EmployeeEntityToDto {
         employeesResponseDto.setId(employeeEntity.getId());
         employeesResponseDto.setPosition(employeeEntity.getPosition());
         employeesResponseDto.setSalary(employeeEntity.getSalary());
-        String employee = employeeEntity.getUser().getPassports().stream().findFirst().map(PassportEntity::getFirstName).orElse("No name found");
-        employeesResponseDto.setFirstName(employee);
+        Long employee = employeeEntity.getUser().getPassports().stream().findFirst().map(PassportEntity::getId).orElseThrow(() -> new IllegalArgumentException("No passport found"));
+        employeesResponseDto.setId(employee);
         return employeesResponseDto;
 
     }
